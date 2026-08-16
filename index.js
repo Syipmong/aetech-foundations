cb9f8ec79bc0d1ee2b7cb12a51837f057ed7409e
Yipmong Said <Syipmong5@gmail.com>
2026-08-16 08:10:02 +0100

    feat: implement sum function logic to return the addition of two numbers

--- index.js
+++ index.js
@@ -2,8 +2,7 @@
 // Your task is to implement the function `sum` that takes two numbers and returns their sum.
 
 function sum(a, b) {
-  // TODO: Implement this function
-  return 0;
+  return a + b;
 }
 
 module.exports = { sum };
